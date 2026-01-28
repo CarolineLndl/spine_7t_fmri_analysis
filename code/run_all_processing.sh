aefede6879482fc85cb1ae8a3fc7ee49ec56bb16
Alexandre D'Astous <po09i@hotmail.com>
2026-01-28 17:01:19 -0500

    Add --no-overwrite option

--- code/run_all_processing.sh
+++ code/run_all_processing.sh
@@ -9,6 +9,8 @@ IDs=() # empty  → process all participants
 TASKS=() # empty → process all tasks
 RUN_PREPROSS=true
 RUN_DENOISING=true
+RUN_FIGURES=true
+REDO=true
 
 # Parse arguments
 while [[ $# -gt 0 ]]; do
@@ -19,22 +21,23 @@ while [[ $# -gt 0 ]]; do
         --tasks) shift; while [[ $# -gt 0 && "$1" != --* ]]; do TASKS+=("$1"); shift; done ;;
         --no-preprocess) RUN_PREPROSS=false; shift;;
         --no-denoising) RUN_DENOISING=false; shift;;
+        --no-figures) RUN_FIGURES=false; shift;;
+        --no-overwrite) REDO=false; shift;;
         *) echo "Unknown argument $1"; exit 1 ;;
     esac
 done
 
 # Show participants
 [ ${#IDs[@]} -eq 0 ] && echo "No specific IDs provided: processing all participants" \
-                     || echo "Processing participants: ${IDs[@]}"
+                     || echo "Processing participants: ${IDs[*]}"
 [ ${#IDs[@]} -eq 0 ] && IDs=("") # If no IDs were provided, set to empty string
-
 
 
 if [ ${#TASKS[@]} -eq 0 ]; then
     echo "No task specified: processing all tasks"
     TASKS_ARG=()        # do not pass --task
 else
-    echo "Processing tasks: ${TASKS[@]}"
+    echo "Processing tasks: ${TASKS[*]}"
     TASKS_ARG=(--tasks "${TASKS[@]}")
 fi
 
@@ -54,7 +57,7 @@ timestamp=$(date +"%Y%m%d_%H%M%S")
 
 if [ "${RUN_PREPROSS}" = true ]; then
     echo "Starting preprocessing..."
-    nohup python -u ../code/preprocessing_workflow.py --path-data "${PATH_DATA}"  --ids "${IDs[@]}" "${TASKS_ARG[@]}" --redo True \
+    nohup python -u ../code/preprocessing_workflow.py --path-data "${PATH_DATA}"  --ids "${IDs[@]}" "${TASKS_ARG[@]}" --redo "${REDO}" \
     > "nohup_preprocessing_${timestamp}.out" 2>&1 &
 
     PID=$!
@@ -70,12 +73,29 @@ fi
 
 if [ "${RUN_DENOISING}" = true ]; then
     echo "Starting denoising..."
-    nohup python -u ../code/denoising_workflow.py --path-data "${PATH_DATA}" --ids "${IDs[@]}" "${TASKS_ARG[@]}" --redo True \
+    nohup python -u ../code/denoising_workflow.py --path-data "${PATH_DATA}" --ids "${IDs[@]}" "${TASKS_ARG[@]}" --redo "${REDO}" \
     > "nohup_denoising_${timestamp}.out" 2>&1 &
     
     PID=$!
     echo "Denoising launched in background."
     echo "Log file: log/nohup_denoising_${timestamp}.out"
+    echo "To stop the process, run:"
+    echo "kill ${PID}"
+fi
+
+
+# --------------------------
+# Run figures
+# --------------------------
+
+if [ "${RUN_FIGURES}" = true ]; then
+    echo "Starting figure generation..."
+    nohup python -u ../code/figure_workflow.py --path-data "${PATH_DATA}" --ids "${IDs[@]}" "${TASKS_ARG[@]}" --redo "${REDO}" \
+    > "nohup_figures_${timestamp}.out" 2>&1 &
+
+    PID=$!
+    echo "Figure generation launched in background."
+    echo "Log file: log/nohup_figures_${timestamp}.out"
     echo "To stop the process, run:"
     echo "kill ${PID}"
 fi
